45b75e5b5ee025b63bb401717797449eca8b38f5
Lucas Marchiori <sinonsrt@hotmail.com>
2020-06-14 17:30:47 -0300

    Trabalho 2 - 2° BM - Banco de Dados

---
+++ core/route.php
@@ -0,0 +1,17 @@
+<?php
+
+$route[]=['/','dashController'];
+$route[]=['usuarios','usuarios/tableview'];
+$route[]=['marcas','marcas/tableview'];
+$route[]=['produtos','produtos/tableview'];
+$route[]=['usuarios_add','usuarios/add'];
+$route[]=['marcas_add','marcas/add'];
+$route[]=['produtos_add','produtos/add'];
+
+
+
+
+
+
+
+//return $route;
---
+++ views/marcas/marcas.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <table class="table">
+    <thead>
+        <tr>
+      
+        <th scope="col">Marca</th>
+        <th scope="col">Descrição</th>
+        <th scope="col"> </th>
+        </tr>
+    </thead>
+    <tbody>
+   
+
+        <tr>
+        
+        <td>dfasd</td>
+        <td>dfa</td>
+        <td>asdf</td>
+        <td><a href="editar?id=" class="bnt">Editar</a></td>
+        </tr>
+        <tr>
+        
+        
+    </tbody>
+    </table>
+    <div class="float-right"><a href="marcas_add"><img src="assets/svg/adicionar.svg" style="width:4em;height:auto"> </a></div>
+</div>
